6abf92ddfcbf36a4120a078e22d414641b280dfd
Admin <vanpho02@gmail.com>
2017-01-04 08:47:04 +0700

    redux

--- app/app.js
+++ app/app.js
@@ -1,7 +1,26 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var redux = require('redux');
 var About = require('./Components/About.js');
 ReactDOM.render(
   <About/>,
   document.getElementById('root')
 );
+
+// var obj = {a: 100, b: 'Pho'}
+// var obj1 = Object.assign({}, obj);
+// var obj1 = {...obj, a: 80}
+// obj.a = 111;
+// console.log(obj1);
+//pure function
+// function add(a, b){
+//   return a + b;
+// }
+
+var reducer = (state = {nhietDo: 30}, action) => {
+  return state;
+}
+
+var store = redux.createStore(reducer);
+
+console.log(store.getState());
---
+++ spread.js
@@ -0,0 +1,3 @@
+var mang = [1, 4, 2];
+var arr = [3, 4, ...mang, 9, 6, 4];
+console.log(arr);
